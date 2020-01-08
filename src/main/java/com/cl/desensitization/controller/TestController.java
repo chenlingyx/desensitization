3f02fc975bce84c5b3242e4ac92f747d69b2e0a5
chenling <chenling@deepexi.com>
2020-01-08 09:36:22 +0800

    增加接口

--- src/main/java/com/cl/desensitization/controller/TestController.java
+++ src/main/java/com/cl/desensitization/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.cl.desensitization.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.cl.desensitization.entity.User;
 import com.cl.desensitization.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +35,24 @@ public class TestController {
     @PostMapping("/add")
     public boolean  insert( @RequestBody  User  user){
         return  testServiceImpl.save(user);
+    }
+
+
+
+
+    @GetMapping("/getOne")
+    public User getOne(Integer id){
+        LambdaQueryWrapper<User> lambda = new QueryWrapper<User>().lambda();
+        lambda.select(User::getPassword).eq(User::getId,id);
+         return testServiceImpl.getOne(lambda);
+    }
+
+
+    @GetMapping("/find")
+    public User getOneByPwd(Integer id,String passsword){
+        LambdaQueryWrapper<User> lambda = new QueryWrapper<User>().lambda();
+        lambda.select(User::getPassword).eq(User::getId,id).eq(User::getPassword,passsword);
+        return testServiceImpl.getOne(lambda);
     }
 
 }
